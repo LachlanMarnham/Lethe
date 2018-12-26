4d6104eb3112c8135f00d895637a2ecd897c50ea
LachlanMarnham <lachlan.marnham@gmail.com>
2018-12-26 10:39:48 +0000

    Add outline of final code structure

--- cmd/lethe/lethe.go
+++ cmd/lethe/lethe.go
@@ -6,10 +6,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, world!")
 	var (
-		string master_password
-		string domain
+		master_password string
+		domain string
 	)
 	master_password, domain = cli.GetSecrets()
+	// password = password_factory.Make(master_password, domain)
+	// if flag not set:
+	// 		cli.SendToStdout(password)
+	// else:
+	// 		cli.SendToClipboard(password)
+	fmt.Println("Domain:", domain)
+	fmt.Println("Master password:", master_password)
+
 }
