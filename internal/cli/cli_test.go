2531ea2375b17704bfd057f59c9d5bf2a1980e4b
LachlanMarnham <lachlan.marnham@gmail.com>
2019-01-03 02:43:47 +0000

    Add testing of getDomain

--- internal/cli/cli_test.go
+++ internal/cli/cli_test.go
@@ -2,11 +2,12 @@ package cli
 
 import (
 	"testing"
+	"bufio"
 	"golang.org/x/crypto/ssh/terminal"
 	"bou.ke/monkey"
 )
 
-func TestMasterPassword(t *testing.T) {
+func TestGetMasterPassword(t *testing.T) {
 	var content string = "myPassWord31%$"
 
 	// Monkeypatch terminal input with content converted to uint8 array 
@@ -19,5 +20,22 @@ func TestMasterPassword(t *testing.T) {
 	master_password := getMasterPassword()
 	if master_password != content {
 		t.Errorf("Unexpected master password. Expected: %s. Got: %s.", content, master_password)
+	}
+}
+
+func TestGetDomain(t *testing.T) {
+	var content string = "google.com"
+
+	// Monkeypatch stdin with content converted to uint8 string 
+	newReadString := func(_ *bufio.Reader, _ uint8) (string, error) {
+		return content + "\n", nil
+	}
+
+	monkey.Patch((*bufio.Reader).ReadString, newReadString)
+	defer monkey.Unpatch((*bufio.Reader).ReadString)  // clean-up
+
+	domain := getDomain()
+	if domain != content {
+		t.Errorf("Unexpected domain. Expected: %s. Got: %s.", content, domain)
 	}
 }
